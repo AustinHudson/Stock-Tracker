e25d0acf2c96ac786a20b38d9f339b85ce750cc2
AustinHudson <hudsaust@oregonstate.edu>
2018-08-24 13:48:54 -0500

    Successful call to global stock data

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,7 +1,12 @@
 import config
+import requests
 
 def get_basic_info(stock):
-    return None
+    query_string = "function=GLOBAL_QUOTE&symbol=" + stock + "&apikey=" + config.api_key
+    info = requests.get("https://www.alphavantage.co/query?" + query_string)
+    return info.text
+
+
 
 
 def calculate_score():
