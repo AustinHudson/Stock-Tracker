eda47c3a0efbdb58ce0f268ef21c02c318d9d67e
AustinHudson <hudsaust@oregonstate.edu>
2018-08-24 13:08:33 -0500

    Form to submit stock name set up as well as routes to display data page

--- Stock_Tracker.py
+++ Stock_Tracker.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from stock_data import get_nasdaq_stocks
+from stock_analysis import get_basic_info
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def getStock():
 
-@app.route('/testdata')
-def getData():
+    return render_template('index.html', nasdaq_stocks=get_nasdaq_stocks())
 
-    return render_template('test.html', nasdaq_stocks=get_nasdaq_stocks())
+
+@app.route('/showData', methods=['POST', 'GET'])
+def show_data():
+    if request.method == 'POST':
+        stockName = request.form['stockName']
+        return render_template("showData.html", stock_name=stockName, basic_info=get_basic_info(stockName))
+
+
 
 
 if __name__ == '__main__':
---
+++ stock_analysis.py
@@ -0,0 +1,10 @@
+import config
+
+def get_basic_info(stock):
+
+
+
+def calculate_score():
+    current_score = 0
+
+
